26abd4cb4b6c51e3f24f994b5f9b7c2da6501a09
peynadol <peynadol@gmail.com>
2025-07-09 09:45:51 +0100

    add title and favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
   import { Toaster } from "svelte-5-french-toast";
 </script>
 
+<svelte:head>
+  <title>Liam Peynado – Portfolio</title>
+</svelte:head>
+
 <main class="min-h-screen bg-background text-foreground">
   <div class="px-6 md:px-8 max-w-4xl mx-auto">
     <slot />
